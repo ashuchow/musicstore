49dc67cb7592902b60e2b4c3f1c77d0a2fb24e23
Aditya Sarkar <adityavsarkar@gmail.com>
2021-01-07 20:13:40 +0530

    first commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,22 @@
+import React, {Component} from "react";
 import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+    <header className = "header">
+        <h1>Musik Mart</h1>
+    </header>
+    <nav>
+    <a href="#">Home</a>
+    <a href="#">Products</a>
+    <a href="#">Contact</a>
+    <a href="#">Cart</a>
+    <a href="#">Login/SignUp</a>
+    </nav>
+  </div>
   );
 }
 
