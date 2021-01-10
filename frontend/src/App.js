5d65741ace48989f6ad07c200ae789339c3cd0a8
Aditya Sarkar <adityavsarkar@gmail.com>
2021-01-11 04:15:12 +0530

    Main Format for Project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,22 @@ import React, {Component} from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-import Home from "./Screens/Home";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import ProductList from "./components/ProductList";
+import Details from "./components/Details";
+import Cart from "./components/Cart";
+import Default from "./components/Default";
+
 
 function App() {
   return (
   <Router>
     <Switch>
-      <Route path="/" exact component={Home} />
+      <Route exact path="/" exact component={Home} />
+      <Route path="/details" component={Details} />
+      <Route path="/cart" component={Cart} />
+      <Route component={Default} />
     
 
     </Switch>
--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,5 +1,7 @@
 import React, {Component} from "react";
+import {Link} from 'react-router-dom';
 import {Button} from 'react-bootstrap';
+
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
