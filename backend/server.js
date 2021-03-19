be694e751f42c2d572ae1d95258b763393760f14
Neil Chowdhary <ashuchow@gmail.com>
2021-03-19 18:25:54 +0530

    small issues, can't post

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,7 @@ const PORT = 5000;
 
 app.use(cors());
 app.use(express.json());
+
 const uri = process.env.ATLAS_URI;
 mongoose.connect(
     uri,
@@ -21,6 +22,22 @@ mongoose.connect(
       console.log("Users Database (MongoDB) is now connected");
     }
   );
+
+  const Product = require("./products");
+
+  app.post("/addproduct", (req, res) => {
+  
+    const newProduct = new Product({
+      name: req.body.name,
+      description: req.body.description,
+    });
+    newProduct.save()
+    .then(() => res.json('Product added!!'))
+    .catch(err => res.status(400).json('Error: ' + err));
+  
+});
+
+
 app.listen(PORT, () =>{
     console.log(`Server is running on port: ${PORT}`)
 })
